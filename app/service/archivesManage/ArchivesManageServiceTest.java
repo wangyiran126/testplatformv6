e0d4cab3b5ed61e53eb058ce55add653f39e1cf9
王奕然 <744382100@qq.com>
2016-10-03 06:11:29 +0800

    性能测试

--- app/service/archivesManage/ArchivesManageServiceTest.java
+++ app/service/archivesManage/ArchivesManageServiceTest.java
@@ -1,10 +1,50 @@
 package service.archivesManage;
 
 import org.junit.runner.RunWith;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by wangyiran on 30/9/2016.
  */
-//@RunWith(SpringJu)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@BenchmarkMode(Mode.AverageTime)
+@OperationsPerInvocation(ArchivesManageServiceTest.times)
 public class ArchivesManageServiceTest {
+    public final static int times = 10000;
+
+    static {
+        System.out.println("hello");
+    }
+
+    @Benchmark
+    public List<Integer> testList() {
+        List<Integer> list = new ArrayList<>();
+        for (Integer i = 0; i < times; i++) {
+            list.add(i);
+        }
+        return list;
+    }
+
+    @Benchmark
+    public Integer[] testArray() {
+        Integer[] list = new Integer[times];
+        for (Integer i = 0; i < times; i++) {
+            list[0] = i;
+        }
+        return list;
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder().include(ArchivesManageServiceTest.class.getSimpleName()).forks(1).build();
+        new Runner(opt).run();
+    }
 }
--- build.sbt
+++ build.sbt
@@ -32,6 +32,12 @@ libraryDependencies += "org.codehaus.jackson" % "jackson-mapper-asl" % "1.9.13"
 
 libraryDependencies += "com.fasterxml.jackson.core" % "jackson-core" % "2.8.2"
 
+// https://mvnrepository.com/artifact/org.springframework/spring-test
+libraryDependencies += "org.springframework" % "spring-test" % "4.3.3.RELEASE"
+
+//性能测试工具
+libraryDependencies += "org.openjdk.jmh" % "jmh-core" % "1.14.1"
+
 routesGenerator := InjectedRoutesGenerator
 
 unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
