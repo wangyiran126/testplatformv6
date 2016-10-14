a196ab7d5d696d7ef998e574d3b9a6e0b9d8f4f6
王奕然 <744382100@qq.com>
2016-10-14 16:36:59 +0800

    docker

---
+++ app/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by wangyiran on 14/10/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
